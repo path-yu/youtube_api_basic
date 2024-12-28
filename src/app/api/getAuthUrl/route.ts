d34ce12713ed695dd2c6e03383a24f05b653a607
path-yu <1974675011@qq.com>
2024-12-28 18:54:47 +0800

    update

--- src/app/api/getAuthUrl/route.ts
+++ src/app/api/getAuthUrl/route.ts
@@ -6,6 +6,7 @@ export async function GET(request: Request) {
   const url = oauth2Client.generateAuthUrl({
     access_type: "offline",
     redirect_uri: process.env.REDIRECT_URL,
+    client_id: process.env.CLIENT_ID,
     scope: ["  https://www.googleapis.com/auth/youtube.readonly"],
   });
   return Response.json({ url });
