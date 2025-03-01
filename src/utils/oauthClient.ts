16361895d1390355bfa5fcb8698c81d2c9907365
path-yu <1974675011@qq.com>
2025-03-01 23:16:50 +0800

    update

--- src/utils/oauthClient.ts
+++ src/utils/oauthClient.ts
@@ -1,7 +1,7 @@
 import { google } from "googleapis";
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URL = process.env.REDIRECT_URL;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET;
+const REDIRECT_URL = process.env.NEXT_PUBLIC_REDIRECT_URI;
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
