ba59745af4f2c05535221f0da799bda4b3ff9623
path-yu <1974675011@qq.com>
2025-03-04 22:29:56 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,16 @@ import CircularProgress from "@mui/material/CircularProgress";
 export default function Home() {
   const [access_token, setToken] = useState("");
   const router = useRouter();
-
+  const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET;
+  const REDIRECT_URL = process.env.NEXT_PUBLIC_REDIRECT_URI;
   useEffect(() => {
     // 在开发环境下，默认跳转到首页
     const isDev = process.env.NODE_ENV === "development";
+    console.log(CLIENT_ID);
+    console.log(CLIENT_SECRET);
+    console.log(REDIRECT_URL);
+
     if (isDev) {
       router.push("/home");
     } else {
